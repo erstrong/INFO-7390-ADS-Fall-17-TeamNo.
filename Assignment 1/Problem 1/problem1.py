b9e7f876ebe148a1198441b8d76ae8bd3cce9ac4
Emily Strong <erstrong.06@gmail.com>
2017-10-04 12:30:38 -0400

    AWS pipeline for problem 1

--- Assignment 1/Problem 1/problem1.py
+++ Assignment 1/Problem 1/problem1.py
@@ -1,5 +1,3 @@
-#!/usr/local/bin/python3
-
 # Working with Edgar datasets: Wrangling, Pre-processing and exploratory data analysis
 
 from lxml import html
@@ -12,18 +10,38 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 import sys
+import logging
+import zipfile
+import boto
+
+# Configure log file
+
+logger = logging.getLogger('problem1')
+hdlr = logging.FileHandler('problem1.log') # set the log file name
+hdlr.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s')) # set the format for the log file
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO) # enable info logging
 
 
-# Input variables (need to set as environmental variables for Docker pipline)
+# Input variables
 
-cik = '51143'
-acc_no = '0000051143-13-000007'
+cik = sys.argv[1] #'51143'
+logger.info('cik: ' + cik)
+
+acc_no = sys.argv[2] #'0000051143-13-000007'
+logger.info('acc_no: ' + acc_no)
+
 acc_short = acc_no[0:10] + acc_no[11:13] + acc_no[14:]
+logger.info('acc_short generated')
 
+AWS_ACCESS_KEY_ID =sys.argv[3]
+AWS_SECRET_ACCESS_KEY=sys.argv[4]
+logger.info('aws keys received')
 
 # Generate full URL and get the page
 
 page = requests.get('https://www.sec.gov/Archives/edgar/data/' + cik + '/' + acc_short + '/' + acc_no +'-index.htm')
+logger.info('data page accessed')
 source = page.content
 
 
@@ -37,20 +55,25 @@ for elem in htmlcontent.iterlinks():
         href+= elem[2]
 
 url10q = 'https://www.sec.gov' + href
+logger.info('10-Q url generated: ' + url10q)
 
 # Scrape Tables with BeautifulSoup
 
 soup = BeautifulSoup(requests.get(url10q).text, 'lxml')
-csvout  = csv.writer(sys.stdout)
+logger.info('10-Q page accessed')
 
 tables=soup.findAll('table')
+logger.info('tables scraped')
 
 # Iterate through tables and generate csvs
 table_count=0
+zipname = "10QTables_" + acc_no + ".zip"
+z = zipfile.ZipFile(zipname, "w")
 
 for table in tables:
+    logger.info('cleaning table...')
     rows = table.findAll('tr')
-    clean_table = [] 
+    clean_table = []
     
     for row in rows:
         clean_row = []
@@ -63,5 +86,32 @@ for table in tables:
     #print(frame)
     csv_name = acc_no + "_" + str(table_count) + ".csv"
     frame.to_csv(csv_name)
+    # logger.info(csv_name + ' created')
+    z.write(csv_name)
+    logger.info(csv_name + ' added to zip file')
     table_count +=1
 
+# Zip log file with the csvs
+
+z.write('problem1.log')
+z.close()
+
+# Post zip file to AWS S3
+
+connection = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
+bucket_name = AWS_ACCESS_KEY_ID.lower() + '-output'
+
+loc = location=boto.s3.connection.Location.DEFAULT
+
+bucket = connection.create_bucket(bucket_name, location=loc)
+
+def percent_cb(complete, total):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+
+s3key = boto.s3.key.Key(bucket)
+s3key.key = zipname
+s3key.set_contents_from_filename(zipname, cb=percent_cb, num_cb=10)
+print("success")
