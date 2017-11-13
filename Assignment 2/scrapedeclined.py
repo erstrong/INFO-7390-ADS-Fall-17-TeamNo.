9b800a7b3201b90ee705d9c8ffac13df530fd4df
Emily Strong <erstrong.06@gmail.com>
2017-11-12 20:59:58 -0500

    Scrape hidden div

--- Assignment 2/scrapedeclined.py
+++ Assignment 2/scrapedeclined.py
@@ -4,20 +4,35 @@ from io import StringIO,BytesIO
 import pandas as pd
 import configparser
 import datetime
+from bs4 import BeautifulSoup
 
 
-base2 = 'https://resources.lendingclub.com/RejectStats'
+# Get the list of files to read
+page='https://www.lendingclub.com/info/download-data.action'
 
-suffix2=['A','B','D','_2016Q1','_2016Q2','_2016Q3','_2016Q4','_2017Q1','_2017Q2','_2017Q3']
+soup = BeautifulSoup(requests.get(page).text, 'lxml')
 
+div =soup.find('div',id='rejectedLoanStatsFileNamesJS')
+div = str(div)
+t = div.split(">")
+t1=t[1].split("<")
+t2 = t1[0].split("|")
+suffix2 = t2[0:len(t2)-1]
+
+
+base = 'https://resources.lendingclub.com/'
+
+#suffix2=['A','B','D','_2016Q1','_2016Q2','_2016Q3','_2016Q4','_2017Q1','_2017Q2','_2017Q3']
+
+# Iterate through the list and read the csv into a data frame
 dataframe_collection2 = []
 
 for suffix in suffix2:
-    url = base2 + suffix + '.csv.zip'
+    url = base + suffix
     print(url)
     r = requests.get(url, stream=True)
     z = zipfile.ZipFile(BytesIO(r.content))
-    df = pd.read_csv(z.open('RejectStats' + suffix + '.csv'),header=1,low_memory=False)
+    df = pd.read_csv(z.open(suffix[:-4]),header=1,low_memory=False)
     dataframe_collection2.append(df)
 
 merged2 = pd.DataFrame(columns=dataframe_collection2[0].columns.values.tolist())
--- Assignment 2/scrapeloans.py
+++ Assignment 2/scrapeloans.py
@@ -4,21 +4,34 @@ from io import StringIO,BytesIO
 import pandas as pd
 import configparser
 import datetime
+from bs4 import BeautifulSoup
 
-base1 = 'https://resources.lendingclub.com/LoanStats'
+# Get the list of files to read
+page='https://www.lendingclub.com/info/download-data.action'
 
-suffix1=['3a','3b','3c','3d','_2016Q1','_2016Q2','_2016Q3','_2016Q4','_2017Q1','_2017Q2','_2017Q3']
+soup = BeautifulSoup(requests.get(page).text, 'lxml')
 
 
 
+div =soup.find('div',id='loanStatsFileNamesJS')
+div = str(div)
+t = div.split(">")
+t1=t[1].split("<")
+t2 = t1[0].split("|")
+suffix1 = t2[0:len(t2)-1]
+#suffix1=['3a','3b','3c','3d','_2016Q1','_2016Q2','_2016Q3','_2016Q4','_2017Q1','_2017Q2','_2017Q3']
+
+base1 = 'https://resources.lendingclub.com/'
+
+# Iterate through the list and read the csv into a data frame
 dataframe_collection = []
 
 for suffix in suffix1:
-  url = base1 + suffix + '.csv.zip'
+  url = base1 + suffix
   print(url)
   r = requests.get(url, stream=True)
   z = zipfile.ZipFile(BytesIO(r.content))
-  df = pd.read_csv(z.open('LoanStats' + suffix + '.csv'),header=1,low_memory=False)
+  df = pd.read_csv(z.open(suffix[:-4]),header=1,low_memory=False)
   dataframe_collection.append(df)
 
 merged = pd.DataFrame(columns=dataframe_collection[0].columns.values.tolist())
